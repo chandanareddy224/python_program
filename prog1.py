3f77f72f562380b866f3fbe62d1faf5b71ba5727
chandana <chandanareddy.alla224@gmail.com>
2026-04-07 22:49:14 +0530

    added even program

--- prog1.py
+++ prog1.py
@@ -3,5 +3,14 @@ def leap_year(n):
     if (n%4==0 or n%400==0):
         print(n,"leap year")
     else:
-        print(n," leap year")
+        print(n,"not a leap year")
 leap_year(2024)
+
+
+
+def even_or_odd(n):
+    if n%2==0:
+        print(n,"even")
+    else:
+        print(n,"odd")
+even_or_odd(33)
